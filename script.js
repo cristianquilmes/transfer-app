3d7deabed8be7697da9a7599a1c3f9a66e5a3235
cristianquilmes <ariazcj@gmail.com>
2025-06-13 21:32:14 +0100

    v4.2

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ scanBtn.addEventListener("click", async () => {
   if (devices.length > 1) {
     await html5QrCode.start(
       devices[2].id,
-      { fps: 10, qrbox: { width: 400, height: 100 } },
+      { fps: 10,
+       aspectRatio: 4.0 // Ancho 4x más que alto (ideal para códigos de barra)
+         },
       (decodedText) => {
         transferInput.value = decodedText;
         html5QrCode.stop();
